14e8709b6eb726a4dd55023b64c76057cf918692
JiYoo <kongsh621@naver.com>
2024-11-27 22:41:42 +0900

    jy commit

---
+++ src/main/java/com/example/sjhealthy/entity/RecommendEntity.java
@@ -0,0 +1,81 @@
+package com.example.sjhealthy.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "recommend_table")
+public class RecommendEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long recId;
+
+    @Column(columnDefinition = "VARCHAR(20)", nullable = false)
+    private String memberId;
+
+    @Column(columnDefinition = "VARCHAR(20)", nullable = false)
+    private String recStore;
+
+    @Column(columnDefinition = "VARCHAR(30)", nullable = false)
+    private String recMenu;
+
+    @Column(columnDefinition = "NUMERIC(18,0) DEFAULT 0", nullable = false)
+    private Long recY;
+
+    @Column(columnDefinition = "NUMERIC(18,0) DEFAULT 0", nullable = false)
+    private Long recN;
+
+    @Column(columnDefinition = "NUMERIC(18,0) DEFAULT 0", nullable = false)
+    private Long recViews;
+
+    @Column(columnDefinition = "VARCHAR(1) DEFAULT 'N'", nullable = false)
+    private String isDeleted;
+
+    @Column(columnDefinition = "VARCHAR(8)", nullable = false)
+    private String createDate;
+
+    @Column(columnDefinition = "VARCHAR(8)", nullable = false)
+    private String updateDate;
+
+    @Column(columnDefinition = "VARCHAR(500)", nullable = false)
+    private String createUser;
+
+    @Column(columnDefinition = "VARCHAR(500)", nullable = false)
+    private String updateUser;
+
+    @PrePersist
+    public void prePersist(){
+        if (this.createDate == null) {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyMMdd");
+            this.createDate = LocalDate.now().format(formatter);
+        }
+
+        if (this.updateDate == null) this.updateDate = this.createDate;
+
+        if (this.createUser == null){
+            this.createUser = this.memberId != null ? this.memberId : "defaultUser";
+            this.updateUser = this.memberId != null ? this.memberId : "defaultUser";
+        }
+
+        if (!this.createUser.equals(this.memberId)){
+            this.updateUser = this.memberId != null ? this.memberId : "defaultUser";
+        }
+
+        if (this.isDeleted == null){
+            this.isDeleted = "N";
+        }
+    }
+
+    @PreUpdate
+    public void preUpdate(){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
+        this.updateDate = LocalDate.now().format(formatter);
+    }
+}
--- src/main/java/com/example/sjhealthy/repository/BoardRepository.java
+++ src/main/java/com/example/sjhealthy/repository/BoardRepository.java
@@ -8,5 +8,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface BoardRepository extends JpaRepository<BoardEntity, Long> {
-
 }
