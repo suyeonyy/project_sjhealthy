cf4a4001afb1b9a6484210a4fe1538fb4d386063
JiYoo <kongsh621@naver.com>
2024-12-03 16:53:37 +0900

    jy commit

--- src/main/java/com/example/sjhealthy/dto/PlaceRequest.java
+++ src/main/java/com/example/sjhealthy/dto/PlaceRequest.java
@@ -4,6 +4,6 @@ import lombok.Data;
 
 @Data
 public class PlaceRequest {
-    private Long storeId; // 가게 고유번호, 기본은 고유번호로 연결
+    private String storeId; // 가게 고유번호, 기본은 고유번호로 연결
     private String placeName; // 프랜차이즈에 적용하기 위해 이름도 받음
 }
