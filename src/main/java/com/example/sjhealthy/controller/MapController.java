f52f555428167cdd794b924e8fad22fc3b24de85
JiYoo <kongsh621@naver.com>
2024-12-03 14:26:03 +0900

    jy commit

--- src/main/java/com/example/sjhealthy/controller/MapController.java
+++ src/main/java/com/example/sjhealthy/controller/MapController.java
@@ -1,13 +1,19 @@
 package com.example.sjhealthy.controller;
 
+import com.example.sjhealthy.dto.PlaceRequest;
+import com.example.sjhealthy.dto.RecommendDTO;
+import com.example.sjhealthy.entity.RecommendEntity;
 import com.example.sjhealthy.service.MapService;
+import com.example.sjhealthy.service.RecommendService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Set;
 
 @RequestMapping("/sjhealthy/")
 @Controller
@@ -15,10 +21,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class MapController {
     //아래 final + RequiredArgsConstructor 적으면 autowird 안적어도됌
     private final MapService mapService;
+    private final RecommendService recommendService;
 
 
     @GetMapping("/map")
-    public String getMapForm(){
+    public String getMapForm(Model model){
+        List<RecommendDTO> recommendList = recommendService.getList();
+        model.addAttribute("recommendDTO", recommendList);
+
         return "map";
     }
 
@@ -36,5 +46,17 @@ public class MapController {
         return mapService.getSearchMapList(longitude, latitude, page, "1");
     }
 
+    @ResponseBody
+    @PostMapping("/map/getrecommend")
+    public ResponseEntity<Set<RecommendEntity>> getRecommendList(@RequestBody PlaceRequest placeRequest){
+
+        Long storeId = placeRequest.getStoreId();
+        String placeName = placeRequest.getPlaceName();
+
+        // 두 가지 조건으로 가져오기 때문에 중복을 없애기 위해 List 말고 Set 사용
+        Set<RecommendEntity> recommendDTOList = recommendService.getListByStoreIdOrPlaceName(storeId, placeName);
+        return ResponseEntity.ok(recommendDTOList);
+
+    }
 
 }
---
+++ src/main/java/com/example/sjhealthy/dto/PlaceRequest.java
@@ -0,0 +1,9 @@
+package com.example.sjhealthy.dto;
+
+import lombok.Data;
+
+@Data
+public class PlaceRequest {
+    private Long storeId; // 가게 고유번호, 기본은 고유번호로 연결
+    private String placeName; // 프랜차이즈에 적용하기 위해 이름도 받음
+}
