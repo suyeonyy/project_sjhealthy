5974cc381498f80bbdd718ece35c7911e9993fb7
SUYEON JUNG <dusl4989@naver.com>
2024-12-05 18:10:02 +0900

    sy commit

--- src/main/java/com/example/sjhealthy/dto/RecommendDTO.java
+++ src/main/java/com/example/sjhealthy/dto/RecommendDTO.java
@@ -6,8 +6,9 @@ import lombok.Data;
 public class RecommendDTO {
     private Long recId;
     private String memberId;
-    private String recStoreId; // 가게 고유 번호
-    private String recStore;
+    private String recStoreId; // 업체 고유 번호
+    private String recStore; //업체 명
+    private String recStoreGroupCode; //업체 그룹 코드
     private String recMenu;
     private String recY; // 좋아요 수
     private String recN; // 싫어요 수
