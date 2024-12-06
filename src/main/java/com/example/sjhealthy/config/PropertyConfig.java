02c2450f281c93284e708ccac82875a85a81c34d
JiYoo <kongsh621@naver.com>
2024-12-07 00:35:32 +0900

    jy commit

---
+++ src/main/java/com/example/sjhealthy/config/PropertyConfig.java
@@ -0,0 +1,14 @@
+package com.example.sjhealthy.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+
+// env.properties 파일을 사용하기 위해 Spring boot에 연결하는 설정 파일
+// https://naturecancoding.tistory.com/96 참고
+@Configuration
+@PropertySources({
+    @PropertySource("classpath:properties/env.properties") // env.properties 파일 소스 등록
+})
+public class PropertyConfig {
+}
