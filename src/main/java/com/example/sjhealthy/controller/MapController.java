c66a77e77499223d70fee2832e9e00c087706ad2
JiYoo <kongsh621@naver.com>
2024-12-07 00:35:32 +0900

    jy commit

--- src/main/java/com/example/sjhealthy/controller/MapController.java
+++ src/main/java/com/example/sjhealthy/controller/MapController.java
@@ -5,6 +5,7 @@ import com.example.sjhealthy.dto.RecommendDTO;
 import com.example.sjhealthy.entity.RecommendEntity;
 import com.example.sjhealthy.service.RecommendService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -19,11 +20,14 @@ public class MapController {
     //아래 final + RequiredArgsConstructor 적으면 autowird 안적어도됌
     private final RecommendService recommendService;
 
+    @Value("${JS_APPKEY}")
+    private String js_appKey;
 
     @GetMapping("/map")
     public String getMapForm(Model model){
         List<RecommendDTO> recommendList = recommendService.getList();
         model.addAttribute("recommendDTO", recommendList);
+        model.addAttribute("js_appKey", js_appKey);
 
         return "map";
     }
