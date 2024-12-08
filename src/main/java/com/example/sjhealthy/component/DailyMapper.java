ad0f09164c74e302c048ae341d08344cd99310e8
JiYoo <kongsh621@naver.com>
2024-12-08 19:38:08 +0900

    jy commit

---
+++ src/main/java/com/example/sjhealthy/component/DailyMapper.java
@@ -0,0 +1,8 @@
+package com.example.sjhealthy.component;
+
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DailyMapper { // 일지
+}
---
+++ src/main/java/com/example/sjhealthy/service/DailyService.java
@@ -0,0 +1,13 @@
+package com.example.sjhealthy.service;
+
+import com.example.sjhealthy.repository.DailyRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class DailyService {
+    private final DailyRepository dailyRepository;
+
+
+}
