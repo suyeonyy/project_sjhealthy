b7af1deed1a7c70f42b61c2fe93e2deecb880b2a
SUYEON JUNG <dusl4989@naver.com>
2024-12-20 23:55:16 +0900

    sy commit

--- src/main/java/com/example/sjhealthy/component/DailyMapper.java
+++ src/main/java/com/example/sjhealthy/component/DailyMapper.java
@@ -9,12 +9,11 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class DailyMapper { // 일지
-    public static DailyEntity toDailyEntity(DailyDTO dailyDTO){
+    public static DailyEntity toDailyEntity(DailyDTO dailyDTO, MemberEntity memberEntity){
         DailyEntity dailyEntity = new DailyEntity();
 
         dailyEntity.setDailyId(dailyDTO.getDailyId());
 //        dailyEntity.setMemberId(dailyDTO.getMemberId());
-        dailyEntity.setMember(MemberMapper.toMemberEntity(dailyDTO.getMember()));
         dailyEntity.setDailyDate(dailyDTO.getDailyDate());
         dailyEntity.setDailyCurWt(dailyDTO.getDailyCurWt());
         dailyEntity.setDailyGoalWt(dailyDTO.getDailyGoalWt());
@@ -27,16 +26,16 @@ public class DailyMapper { // 일지
         dailyEntity.setUpdateDate(dailyDTO.getUpdateDate());
         dailyEntity.setCreateUser(dailyDTO.getCreateUser());
         dailyEntity.setUpdateUser(dailyDTO.getUpdateUser());
+        dailyEntity.setMember(memberEntity);
 
         return dailyEntity;
     }
 
-    public static DailyDTO toDailyDTO(DailyEntity dailyEntity){
+    public static DailyDTO toDailyDTO(DailyEntity dailyEntity, String memberId){
         DailyDTO dailyDTO = new DailyDTO();
 
         dailyDTO.setDailyId(dailyEntity.getDailyId());
 //        dailyDTO.setMemberId(dailyEntity.getMemberId());
-        dailyDTO.setMember(MemberMapper.toMemberDTO(dailyEntity.getMember()));
         dailyDTO.setDailyDate(dailyEntity.getDailyDate());
         dailyDTO.setDailyCurWt(dailyEntity.getDailyCurWt());
         dailyDTO.setDailyGoalWt(dailyEntity.getDailyGoalWt());
@@ -49,6 +48,7 @@ public class DailyMapper { // 일지
         dailyDTO.setUpdateDate(dailyEntity.getUpdateDate());
         dailyDTO.setCreateUser(dailyEntity.getCreateUser());
         dailyDTO.setUpdateUser(dailyEntity.getUpdateUser());
+        dailyDTO.setMemberId(memberId);
 
         return dailyDTO;
     }
