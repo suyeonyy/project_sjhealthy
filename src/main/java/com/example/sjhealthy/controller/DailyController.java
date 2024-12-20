5880c5fffdf4c9615723338993e1fa8995c449e1
SUYEON JUNG <dusl4989@naver.com>
2024-12-20 23:55:16 +0900

    sy commit

--- src/main/java/com/example/sjhealthy/controller/DailyController.java
+++ src/main/java/com/example/sjhealthy/controller/DailyController.java
@@ -3,9 +3,11 @@ package com.example.sjhealthy.controller;
 import com.example.sjhealthy.dto.BoardDTO;
 import com.example.sjhealthy.dto.DailyDTO;
 import com.example.sjhealthy.dto.MemberDTO;
+import com.example.sjhealthy.entity.MemberEntity;
 import com.example.sjhealthy.service.DailyService;
 import com.example.sjhealthy.service.MemberService;
 import com.sun.source.tree.MemberReferenceTree;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -13,7 +15,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequiredArgsConstructor
@@ -27,7 +31,7 @@ public class DailyController {
                                Model model){
         model.addAttribute("loginId", loginId);
 
-        List<DailyDTO> dailyList = dailyService.getList();
+        List<DailyDTO> dailyList = dailyService.getList(loginId);
         System.out.println("dailyList = " + dailyList);
         model.addAttribute("dailyList", dailyList);
 
@@ -51,14 +55,14 @@ public class DailyController {
     }
 
     @PostMapping("/daily/dailyWrite")
-    public ResponseEntity<String> postDailyWrite(@SessionAttribute(name = "loginId", required = false) String loginId,
-                                 @ModelAttribute DailyDTO dailyDTO, Model model, RedirectAttributes ra){
+    public ResponseEntity<Map<String, String>> postDailyWrite(@SessionAttribute(name = "loginId", required = false) String loginId,
+                                                              @ModelAttribute DailyDTO dailyDTO, Model model, RedirectAttributes ra){
         model.addAttribute("loginId", loginId);
 
-        System.out.println("진입하나용????");
+        MemberEntity memberEntity = memberService.findMemberEntity(loginId);
 
         try {
-            DailyDTO writeResult = dailyService.write(dailyDTO);
+            DailyDTO writeResult = dailyService.write(dailyDTO, memberEntity);
 
             if (writeResult != null) {
                 ra.addFlashAttribute("dailyDTO", writeResult);
@@ -66,20 +70,38 @@ public class DailyController {
 
                 //ra.addAttribute("loginId", loginId); //addAttribute로 보내면 리다이렉트 값 유지못함
                 ra.addFlashAttribute("loginId", loginId);
+                ra.addFlashAttribute("alertMessage", "일지가 등록되었습니다.");
+
+                Map<String, String> response = new HashMap<>();
+                String redirectUrl = "/sjhealthy/daily/dailyList";
+                String alertMessage = "일지가 등록되었습니다."; // 알림 메시지
+                response.put("redirectUrl", redirectUrl);
+                response.put("alertMessage", alertMessage);
 
                 // 리다이렉트 URL을 JSON 형태로 응답
-                return ResponseEntity.ok("/sjhealthy/daily/dailyList");
+                return ResponseEntity.ok(response);
+                //return ResponseEntity.ok("/sjhealthy/daily/dailyList");
                 //return "redirect:/sjhealthy/daily/dailyList";
             } else {
+                Map<String, String> response = new HashMap<>();
+                String redirectUrl = "/sjhealthy/daily/dailyList";
+                response.put("redirectUrl", redirectUrl);
+
                 System.out.println("일지 등록 실패");
-                return ResponseEntity.ok("/sjhealthy/daily/dailyList");
+                return ResponseEntity.ok(response);
+                //return ResponseEntity.ok("/sjhealthy/daily/dailyList");
                 //return "redirect:/sjhealthy/daily/dailyList";
             }
 
         } catch (Exception e){
+            Map<String, String> response = new HashMap<>();
+            String redirectUrl = "/sjhealthy/daily/dailyList";
+            response.put("redirectUrl", redirectUrl);
+
             e.printStackTrace(); //오류 원인 확인용
             System.out.println("시스템 오류로 실패");
-            return ResponseEntity.ok("/sjhealthy/daily/dailyList");
+            return ResponseEntity.ok(response);
+            //return ResponseEntity.ok("/sjhealthy/daily/dailyList");
             //return "redirect:/sjhealthy/daily/dailyList";
         }
     }
