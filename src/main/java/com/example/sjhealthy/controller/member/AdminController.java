5fa7ae04161ebe35f0648467fa853adbf812d209
JiYoo <kongsh621@naver.com>
2024-12-20 14:02:04 +0900

    jy commit

--- src/main/java/com/example/sjhealthy/controller/member/AdminController.java
+++ src/main/java/com/example/sjhealthy/controller/member/AdminController.java
@@ -6,9 +6,13 @@ import com.example.sjhealthy.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 import java.util.List;
 
+@RequestMapping("/sjhealthy/")
 @Controller
 public class AdminController {
     @Autowired
@@ -17,11 +21,25 @@ public class AdminController {
     @Autowired
     private MemberService memberService;
 
-    public String getMemberList(Model model){
-        List<MemberDTO> dto = memberService.getMemberList();
+    @GetMapping("/admin")
+    public String getMemberList(Model model, @SessionAttribute(name = "loginId", required = false)String loginId) {
+        model.addAttribute("loginId", loginId);
+        if (loginId != null) {
+            MemberDTO loginMember = memberService.findMemberIdAtPassFind(loginId);
+            System.out.println(loginMember);
 
-        model.addAttribute("memberList", dto);
-        return "adminPage";
+            if (loginMember.getMemberAuth().equals("A")) {
+                // 관리자인지 확인
+                List<MemberDTO> dto = memberService.getMemberList();
+                model.addAttribute("memberList", dto);
+                return "adminPage";
+            } else {
+                System.out.println("관리자만 접근 가능한 페이지입니다.");
+                return "redirect:/sjhealthy";
+            }
+        } else {
+            System.out.println("관리자만 접근 가능한 페이지입니다.");
+            return "redirect:/sjhealthy/member/login";
+        }
     }
-
 }
