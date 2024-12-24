34daaee79405e2c6e370a395a79d78c0bcf03fad
SUYEON JUNG <dusl4989@naver.com>
2024-12-24 12:34:48 +0900

    sy commit

--- src/main/java/com/example/sjhealthy/dto/DailyDTO.java
+++ src/main/java/com/example/sjhealthy/dto/DailyDTO.java
@@ -8,13 +8,14 @@ import lombok.NoArgsConstructor;
 @Data //@Data: @Getter, @Setter, @ToString, @EqualsAndHashCode, @RequiredArgsConstructor 를 합쳐놓은
 @NoArgsConstructor
 @AllArgsConstructor
-public class DailyDTO { // 일지
+public class DailyDTO {         // 일지
     private Long dailyId;       //글 고유번호
     private String memberId;    //아이디
     //private MemberDTO member;
+    private String dailyTitle;  //제목
     private String dailyDate;   //작성일
-    private double dailyCurWt;     //현재 체중
-    private double dailyGoalWt;    //목표 체중
+    private double dailyCurWt;  //현재 체중
+    private double dailyGoalWt; //목표 체중
     private String dailyGoalSf; //목표 만족도
     private String dailyMemo;   //메모
     private String dailyYear;   //년도
