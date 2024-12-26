ddb6148ea6f459cf82127d440e970a226c7887c6
JiYoo <kongsh621@naver.com>
2024-12-26 16:34:03 +0900

    jy

--- src/main/java/com/example/sjhealthy/repository/DailyRepository.java
+++ src/main/java/com/example/sjhealthy/repository/DailyRepository.java
@@ -68,7 +68,7 @@ public interface DailyRepository extends JpaRepository<DailyEntity, Long> {
     @Query(value= "SELECT " + // 특정 멤버의 목표 달성도, 순위, BMI를 구해 가져옴(해당 회원 통계 출력용)
             "m.member_id AS memberId, " + // memberId
             "((first.daily_cur_wt - d.daily_cur_wt) / (first.daily_cur_wt - d.daily_goal_wt)) * 100 AS memberAchievementPercentage, " +  // 목표 달성도
-            "memberRank AS memberRank, " + // 순위
+            "ranked.memberRank AS memberRank, " + // 순위
             "(10000 * d.daily_cur_wt / (m.member_height * m.member_height)) AS memberBmi " + // BMI
             "FROM daily_table d " + // nativeQuery를 쓰면 테이블로 표기
             "JOIN member_table m ON m.member_id = d.member_id " + // member_table과 조인
@@ -77,14 +77,14 @@ public interface DailyRepository extends JpaRepository<DailyEntity, Long> {
             "       SELECT MIN(daily_id) FROM daily_table WHERE member_id = :memberId) " +
             "JOIN ( " + // 순위 계산
             "       SELECT " +
-            "           d.member_id AS member_id, " +
+            "           d.member_id AS memberId, " +
             "           DENSE_RANK() OVER (ORDER BY ((first.daily_cur_wt - d.daily_cur_wt) / (first.daily_cur_wt - d.daily_goal_wt)) DESC) AS memberRank " +
             "       FROM daily_table d " +
             "       JOIN member_table m ON m.member_id = d.member_id " +
             "       LEFT JOIN daily_table first ON first.member_id = m.member_id AND first.daily_id = ( " +
             "           SELECT MIN(daily_id) FROM daily_table WHERE member_id = m.member_id) " +
             "       WHERE d.daily_id = (SELECT MAX(d2.daily_id) FROM daily_table d2 WHERE d2.member_id = d.member_id) " +
-            ") rank ON rank.member_id = d.member_id " +
+            ") ranked ON ranked.memberId = d.member_id " +
             "WHERE d.member_id = :memberId AND d.daily_id = (SELECT " + // 현재 체중은 멤버별 가장 최근 데이터 사용
             "                       MAX(d2.daily_id) FROM daily_table d2 WHERE d2.member_id = :memberId)"
             , nativeQuery = true)
