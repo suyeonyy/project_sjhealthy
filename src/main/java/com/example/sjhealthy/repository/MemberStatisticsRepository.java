298678a4a34123e66277571c215dc5901b1fb1a3
JiYoo <kongsh621@naver.com>
2025-01-06 19:08:23 +0900

    jy commit

--- src/main/java/com/example/sjhealthy/repository/MemberStatisticsRepository.java
+++ src/main/java/com/example/sjhealthy/repository/MemberStatisticsRepository.java
@@ -4,11 +4,11 @@ import com.example.sjhealthy.entity.DailyEntity;
 import com.example.sjhealthy.entity.MemberStatisticsEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Map;
 
 // 통계, 광장
 public interface MemberStatisticsRepository extends JpaRepository<MemberStatisticsEntity, String> {
-
 }
