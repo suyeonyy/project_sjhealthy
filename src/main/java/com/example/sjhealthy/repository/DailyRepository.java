8d4fca738db6c663ba5806df5592a83b68dfad87
SUYEON JUNG <dusl4989@naver.com>
2025-01-31 14:35:12 +0900

    sy commit

--- src/main/java/com/example/sjhealthy/repository/DailyRepository.java
+++ src/main/java/com/example/sjhealthy/repository/DailyRepository.java
@@ -108,6 +108,7 @@ public interface DailyRepository extends JpaRepository<DailyEntity, Long> {
     //현재 날짜(yyyymmdd)를 기준으로, 현재 월로 등록된 데이터 유무 가져오기
     @Query(value = "SELECT  DTBL.DAILY_DATE " +
             ",DTBL.DAILY_TITLE " +
+            ",DTBL.DAILY_CUR_WT " +
             "FROM  daily_table DTBL " +
             "WHERE  1 = 1 " +
             "AND  DTBL.MEMBER_ID = :loginId " +
