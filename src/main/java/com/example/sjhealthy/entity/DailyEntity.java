407e5e6060950af773b84b9308d731bae1899317
SUYEON JUNG <dusl4989@naver.com>
2025-02-01 15:40:08 +0900

    sy commit

--- src/main/java/com/example/sjhealthy/entity/DailyEntity.java
+++ src/main/java/com/example/sjhealthy/entity/DailyEntity.java
@@ -59,7 +59,7 @@ public class DailyEntity { // 일지
     private String updateUser;
 
     @ManyToOne(fetch = FetchType.LAZY) //일지 기준
-    @JoinColumn(name = "member_id")
+    @JoinColumn(name = "member_id", nullable = false)
     private MemberEntity member; // 참조하여 memberHeight 를 사용
 
     @PrePersist
