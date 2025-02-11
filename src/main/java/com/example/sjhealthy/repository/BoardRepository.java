d1de4f013ab167c9f0a2429ef71ed17740f97641
JiYoo <kongsh621@naver.com>
2025-02-11 15:03:26 +0900

    jy commit

--- src/main/java/com/example/sjhealthy/repository/BoardRepository.java
+++ src/main/java/com/example/sjhealthy/repository/BoardRepository.java
@@ -11,5 +11,5 @@ import java.util.List;
 public interface BoardRepository extends JpaRepository<BoardEntity, Long> {
     List<BoardEntity> findAllByOrderByBoardIdDesc(); // 기존에 쓰던 거
     Page<BoardEntity> findAllByOrderByBoardIdDesc(Pageable pageable); // 페이징 추가한 거
-    List<BoardEntity> findTop5ByOrderByCreateDateDesc();
+    List<BoardEntity> findTop4ByOrderByCreateDateDesc();
 }
